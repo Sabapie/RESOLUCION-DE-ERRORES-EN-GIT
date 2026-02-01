2bbf03992d0198fae3dc006d8a93f3ba1f44f0b2
alejndromorenogarcia-ctrl <alejndromorenogarcia@gmail.com>
2026-02-01 09:02:52 +0100

    fix: DELETE en selecionar.php

--- src/clientes/seleccionar.php
+++ src/clientes/seleccionar.php
@@ -8,7 +8,7 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");  
  
 // Recibimos los datos por GET. 
-header("Access-Control-Allow-Methods: GET"); 
+header("Access-Control-Allow-Methods: DELETE"); 
  
 include_once('../bd/clientes.php'); 
  
